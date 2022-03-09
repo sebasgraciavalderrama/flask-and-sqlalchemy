613eafb5aa144995183c75df6b6aed5cbba9f190
Sebastian Gracia <sebastiangracia123@gmail.com>
2022-03-09 14:06:12 -0500

    code deleted and some bugs squashed

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -6,22 +8,19 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+app.config['DEBUG'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'sebas'
 api = Api(app)
 
-
-@app.before_first_request
-def call_create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)
+
 
 #------ ADDITION OF RESOURCES TO OUR ENDPOINT ------
 api.add_resource(Store, '/store/<string:name>')
@@ -32,5 +31,12 @@ api.add_resource(StoreList, '/stores')
 #---------------------------------------------------
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
     app.run(port=5000, debug=True)  # important to mention debug=True
--- models/store.py
+++ models/store.py
@@ -1,5 +1,3 @@
-import sqlite3
-from flask_restful import Resource
 from db import db
 
 
@@ -27,21 +25,3 @@ class StoreModel(db.Model):
     def delete_from_db(self):
         db.session.delete(self)
         db.session.commit()
-
-
-class ItemList(Resource):
-    def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
-
-        items = []
-
-        for row in result:
-            items.append({'name': row[0], 'price': row[1]})
-
-        connection.close()
-
-        return {'items': items}
