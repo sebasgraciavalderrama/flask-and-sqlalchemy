28eeeab4969212281965a2fdfe474ae1e6ececd9
Sebastian Gracia <sebastiangracia123@gmail.com>
2022-03-09 15:40:42 -0500

    change to env var to be DATABASE_URI

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'sebas'
